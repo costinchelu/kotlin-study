9a5b1f68d4e51f9e5b7ee71eef9336e136c0d95c
Costin C <costin.chelu@gmail.com>
2024-07-03 19:02:16 +0300

    ..

--- kia/src/main/kotlin/ch04/4.1.4_1_InnerAndNestedClasses1.kt
+++ kia/src/main/kotlin/ch04/4.1.4_1_InnerAndNestedClasses1.kt
@@ -1,18 +1,37 @@
-package ch04.Button1
+package ch04.ex1_4_2_InnerAndNestedClasses1
 
 import java.io.Serializable
 
+
+
+class Outer {
+
+    inner class Inner {
+
+        fun getOuterReference(): Outer = this@Outer
+    }
+}
+
+
+
 interface State: Serializable
 
+
 interface View {
+
     fun getCurrentState(): State
+
     fun restoreState(state: State) {}
 }
 
+
 class Button : View {
+
     override fun getCurrentState(): State = ButtonState()
 
     override fun restoreState(state: State) { /*...*/ }
 
+    // Button is not serializable, and it breaks the serialization of ButtonState
+    // to fix the problem we need to declare ButtonState as static
     class ButtonState : State { /*...*/ }
 }
