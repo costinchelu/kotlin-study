2b8b8c77edc3c7f5343b34fdb6bd292b4699c4d3
Costin C <costin.chelu@gmail.com>
2024-07-21 21:48:50 +0300

    ..

--- kia/src/main/kotlin/ch05/5.1.2_LambdasAndCollections.kt
+++ kia/src/main/kotlin/ch05/5.1.2_LambdasAndCollections.kt
@@ -17,4 +17,16 @@ fun findTheOldest(people: List<Person>) {
 fun main() {
     val people = listOf(Person("Alice", 29), Person("Bob", 31))
     findTheOldest(people)
+
+    // or, with lambda:
+    people.maxByOrNull { it.age }?.also { println("Max age is $it") }
+
+
+
+
+    val names = people.joinToString(
+        separator = " ",
+        transform = { p: Person -> p.name }
+    )
+    println(names)
 }
--- kia/src/main/kotlin/ch05/5.1.3_1_SyntaxForLambdaExpressions.kt
+++ kia/src/main/kotlin/ch05/5.1.3_1_SyntaxForLambdaExpressions.kt
@@ -1,6 +1,19 @@
 package ch05.ex1_3_1_SyntaxForLambdaExpressions
 
 fun main() {
+
+    // we can directly run lambda
+    { println(42) }()
+
+    // or
+    run { println(42) }
+
     val sum = { x: Int, y: Int -> x + y }
     println(sum(1, 2))
+
+    val sum2 = {
+        x: Int, y: Int -> println("Computing the sum of $x and $y...")
+        x + y
+    }
+    println(sum2(1, 2))
 }
