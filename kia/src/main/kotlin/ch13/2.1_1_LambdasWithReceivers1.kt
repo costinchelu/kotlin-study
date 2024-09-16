9c8b727e6d5606dad5bf94a47ac04b0f02ba12db
costin.chelu <costin.chelu@johnlewis.co.uk>
2024-09-16 13:40:53 +0300

    ch12.3

---
+++ kia/src/main/kotlin/ch13/2.1_1_LambdasWithReceivers1.kt
@@ -0,0 +1,31 @@
+package ch13.LambdaWithReceivers1
+// Using a regular lambda -> we need to add it. for each operation in the lambda
+fun buildStringRegularLambda(builderAction: (StringBuilder) -> Unit): String {
+    val sb = StringBuilder()
+    builderAction(sb)
+    return sb.toString()
+}
+
+// using lambda with receivers -> no need for it because we are already referring to a StringBuilder
+// because this lambda passes the StringBuilder like in an extension function
+fun buildStringWReceiver(builderAction: StringBuilder.() -> Unit): String {
+    val sb = StringBuilder()
+    sb.builderAction()
+    return sb.toString()
+}
+
+fun main() {
+    val s = buildStringRegularLambda {
+        it.append("Hello, ")
+        it.append("World!")
+    }
+    println(s)                      // Hello, World!
+
+
+    val s2 = buildStringWReceiver {
+//        this.append("Hello, ")  -> default form, referring to the passed StringBuilder
+        append("Hello, ")
+        append("World!")
+    }
+    println(s2)                      // Hello, World!
+}
--- kia/src/main/kotlin/ch13/2.1_2_LambdasWithReceivers2.kt
+++ kia/src/main/kotlin/ch13/2.1_2_LambdasWithReceivers2.kt
@@ -1,4 +1,4 @@
-package ch13.ex1_3_LambdasWithReceivers2
+package ch13.LambdasWithReceivers2
 
 val appendExcl: StringBuilder.() -> Unit = { this.append("!") }
 
@@ -10,8 +10,14 @@ fun main() {
 
     // ----------------------------------------------------
 
+    // apply vs with is another example of functions that invoke the argument of an extension function type on the provided receiver
+    // apply is declared as an extension to that receiver but with takes it as an argument
+    // the main difference is that apply returns the receiver itself but with returns the result of calling the lambda
     val map = mutableMapOf(1 to "one")
+
     map.apply { this[2] = "two" }
+
     with(map) { this[3] = "three" }
+
     println(map)                               // {1=one, 2=two, 3=three}
 }
