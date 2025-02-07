6ae3e8d8c69ddc25f8fd58e9d1ed94ae273339c0
costin.chelu <costin.chelu@johnlewis.co.uk>
2025-02-07 18:26:39 +0200

    Basics

--- kotlin-and-java/src/main/java/ro/ccar/java/BasicsJ.java
+++ kotlin-and-java/src/main/java/ro/ccar/java/BasicsJ.java
@@ -1,23 +1,87 @@
 package ro.ccar.java;
 
+import can.be.named.as_needed.BasicsK;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class BasicsJ {
 
-    /*
-    operands = an object of a mathematical or logical operation.
-        The part of a computer instruction which specifies what data is to be manipulated or operated on, while at the same time, it is representing the data itself:
-    variables = storage location to hold a value - acting like a container for the data
-        values resulted from expressions = combination of literals (constants), variables, operators
-            and function calls that evaluates to a single value
-    literals = equivalent to constants
-    operators = symbols (mathematical, logical, of selection) that can be applied to a set of operands
-    statements = complete unit of execution - manipulating variables, assigning data-type values to them
-        & controlling the flow of execution of such operations
-    declarations = create variables of a specified type and name them with identifiers
-    assignments = associate a data-type value (defined by an expression) with a variable (initialization)
-    control flow structures = change the flow of execution and are used for branching:
-        conditionals = the decision making part of the program
-        loops = repeating blocks of code
-        calls
-        returns
-     */
+    private final int instanceConstant = 3;
+
+    private void methodReturningVoid(String param1, int param2) {
+
+        // variable
+        int x = 1;
+        var x1 = 1;
+
+        // constant
+        final int y = 2;
+    }
+
+    private void instantiationExample() {
+        // instantiating an object from Kotlin code
+        BasicsK basicsK = new BasicsK();
+    }
+
+    private char switchExample(int value) {
+        return switch (value) {
+            case 0, 1, 2, 3, 4 -> 'a';
+            case 5 -> 'b';
+            case 6, 7, 8 -> 'c';
+            case 9 -> 'd';
+            default -> {
+                if (value >= 10 && value <= 20) {
+                    yield 'e';
+                } else {
+                    throw new IllegalArgumentException();
+                }
+            }
+        };
+    }
+
+    private char ifExample(int value) {
+        char result;
+        if (value >= 0 && value < 5) {
+            result = 'a';
+        } else if (value == 5) {
+            result = 'b';
+        } else if (value == 6 || value == 7 || value == 8) {
+            result = 'c';
+        } else if (value == 9) {
+            result = 'd';
+        } else if (value >= 10 && value <= 20) {
+            result = 'e';
+        } else {
+            throw new IllegalArgumentException();
+        }
+        return result;
+    }
+
+    private void forLoopExample() {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        List<Integer> intList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        for (int number: intList) {
+            System.out.println(number);
+        }
+    }
+
+    private void castingExample() {
+        Object obj = "Hello";
+
+        if (obj instanceof String) {
+            String message = (String) obj; // Explicit type casting
+        }
+    }
+
+    private void functionalExample() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        int sum = numbers.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
 }
--- kotlin-and-java/src/main/kotlin/ro/ccar/kt/BasicsK.kt
+++ kotlin-and-java/src/main/kotlin/ro/ccar/kt/BasicsK.kt
@@ -1,4 +1,64 @@
-package ro.ccar.kt
+package can.be.named.as_needed
+
+import ro.ccar.java.BasicsJ
+
+// constant (outside class)
+const val z: Int = 3        // const is used only in compile time
 
 class BasicsK {
+
+    fun methodReturningUnit(param1: String, param2: Int) {
+
+        // variable
+        var x: Int = 1
+        var x1 = 1
+
+        // constant
+        val y: Int = 2          // y is set in runtime
+    }
+
+    fun instantiationExample() {
+        // instantiating an object from Java code
+        val javaObject = BasicsJ()
+    }
+
+    fun whenExample(value: Int): Char {
+        return when (value) {
+            in 0..4 -> 'a'
+            5 -> 'b'
+            6, 7, 8 -> 'c'
+            9 -> 'd'
+            in 10..20 -> 'e'
+            else -> throw Error()
+        }
+    }
+
+    fun forLoopExample() {
+        for ( i  in 0..10 ){
+            println(i)
+        }
+
+        val intList: List<Int> = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+        for (number in intList) {
+            println(number)
+        }
+    }
+
+    fun castingExample() {
+        val obj: Any = "Hello"
+
+        if (obj is String) {
+            val message: String = obj // Automatic type casting
+        }
+    }
+
+    fun functionalExample() {
+        val numbers = listOf(1, 2, 3, 4, 5)
+        val sum = numbers.sum()
+    }
+
+    fun compactFunction1() = println("I am calling the user...");
+
+    fun compactFunction2() = 12;
 }
